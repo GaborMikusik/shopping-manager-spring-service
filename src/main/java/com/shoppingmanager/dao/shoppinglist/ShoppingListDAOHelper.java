d538a1d0056f6294f331a4394563b4b26d3735c3
GaborMikusik <gabor.mikusik@gmail.com>
2023-09-18 20:15:20 +0200

    shopping-list-dao refactor

---
+++ src/main/java/com/shoppingmanager/dao/shoppinglist/ShoppingListDAOHelper.java
@@ -0,0 +1,53 @@
+package com.shoppingmanager.dao.shoppinglist;
+
+import com.shoppingmanager.model.shopping.ShoppingList;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.LinkedList;
+import java.util.List;
+
+public final class ShoppingListDAOHelper {
+    public static List<ShoppingList> getShoppingLists(ResultSet rs) throws SQLException {
+        List<ShoppingList> resultList = new LinkedList<>();
+        while (rs.next()) {
+            ShoppingList shoppingList = getShoppingList(rs);
+            resultList.add(shoppingList);
+        }
+        return resultList;
+    }
+
+    public static ShoppingList getList(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            return getShoppingList(rs);
+        } else {
+            return null;
+        }
+    }
+
+    public static ShoppingList getShoppingList(ResultSet rs) throws SQLException {
+        ShoppingList shoppingList = new ShoppingList();
+        shoppingList.setId(rs.getLong("id"));
+        shoppingList.setName(rs.getString("name"));
+        shoppingList.setDescription(rs.getString("description"));
+        shoppingList.setPaid(rs.getBoolean("paid"));
+        shoppingList.setCreatedAt(convertToInstant(rs, "created_at"));
+        shoppingList.setUpdatedAt(convertToInstant(rs, "updated_at"));
+        shoppingList.setCreatedBy(rs.getLong("created_by"));
+        shoppingList.setUpdatedBy(rs.getLong("updated_by"));
+        return shoppingList;
+    }
+
+    public static Instant convertToInstant(ResultSet rs, String columnName) throws SQLException {
+        Object object = rs.getObject(columnName);
+        if (object instanceof java.sql.Timestamp) {
+            return ((java.sql.Timestamp) object).toInstant();
+        } else if (object instanceof java.sql.Date) {
+            return ((java.sql.Date) object).toLocalDate().atStartOfDay().toInstant((ZoneOffset) ZoneId.of("UTC"));
+        }
+        throw new IllegalArgumentException("Unsupported data type for column: " + columnName);
+    }
+}
--- src/main/java/com/shoppingmanager/dao/shoppinglist/ShoppingListDAOImpl.java
+++ src/main/java/com/shoppingmanager/dao/shoppinglist/ShoppingListDAOImpl.java
@@ -5,12 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.persistence.EntityNotFoundException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ShoppingListDAOImpl implements ShoppingListDAO {
@@ -25,8 +19,7 @@ public class ShoppingListDAOImpl implements ShoppingListDAO {
     @Override
     public List<ShoppingList> getShoppingLists(Long userId) {
         final String query = "SELECT * FROM shopping_list WHERE created_by = ?";
-
-        return getShoppingLists(userId, query);
+        return jdbcTemplate.query(query, ShoppingListDAOHelper::getShoppingLists, userId);
     }
 
     @Override
@@ -38,14 +31,14 @@ public class ShoppingListDAOImpl implements ShoppingListDAO {
     public List<ShoppingList> getActualShoppingLists(Long userId) {
         final String query = "SELECT * FROM shopping_list WHERE created_by = ? AND paid = false";
 
-        return getShoppingLists(userId, query);
+        return jdbcTemplate.query(query, ShoppingListDAOHelper::getShoppingLists, userId);
     }
 
     @Override
     public List<ShoppingList> getAlreadyPaidShoppingLists(Long userId) {
         final String query = "SELECT * FROM shopping_list WHERE created_by = ? AND paid = true";
 
-        return getShoppingLists(userId, query);
+        return jdbcTemplate.query(query, ShoppingListDAOHelper::getShoppingLists, userId);
     }
 
     @Override
@@ -54,7 +47,8 @@ public class ShoppingListDAOImpl implements ShoppingListDAO {
         int rowsUpdated = jdbcTemplate.update(updateQuery, true, listId);
 
         if (rowsUpdated == 1) {
-            return getShoppingListById(listId);
+            final String selectQuery = "SELECT * FROM shopping_list WHERE id = ?";
+            return jdbcTemplate.query(selectQuery, ShoppingListDAOHelper::getList, listId);
         } else {
             throw new EntityNotFoundException("ShoppingList with ID " + listId + " not found");
         }
@@ -70,7 +64,8 @@ public class ShoppingListDAOImpl implements ShoppingListDAO {
                 shoppingList.isPaid(),
                 shoppingList.getId());
         if (rowsUpdated == 1) {
-            return getShoppingListById(shoppingList.getId());
+            final String selectQuery = "SELECT * FROM shopping_list WHERE id = ?";
+            return jdbcTemplate.query(selectQuery, ShoppingListDAOHelper::getList, shoppingList.getId());
         } else {
             throw new EntityNotFoundException("ShoppingList with ID " + shoppingList.getId() + " not found");
         }
@@ -84,50 +79,5 @@ public class ShoppingListDAOImpl implements ShoppingListDAO {
         if (rowsDeleted == 0) {
             throw new EntityNotFoundException("ShoppingList with ID " + id + " not found");
         }
-    }
-
-    private List<ShoppingList> getShoppingLists(Long userId, String query) {
-        return jdbcTemplate.query(query, rs -> {
-            List<ShoppingList> resultList = new LinkedList<>();
-            while (rs.next()) {
-                ShoppingList shoppingList = getShoppingList(rs);
-                resultList.add(shoppingList);
-            }
-            return resultList;
-        }, userId);
-    }
-
-    private ShoppingList getShoppingListById(Long id) {
-        final String selectQuery = "SELECT * FROM shopping_list WHERE id = ?";
-        return jdbcTemplate.query(selectQuery, rs -> {
-            if (rs.next()) {
-                return getShoppingList(rs);
-            } else {
-                return null;
-            }
-        }, id);
-    }
-
-    private ShoppingList getShoppingList(ResultSet rs) throws SQLException {
-        ShoppingList shoppingList = new ShoppingList();
-        shoppingList.setId(rs.getLong("id"));
-        shoppingList.setName(rs.getString("name"));
-        shoppingList.setDescription(rs.getString("description"));
-        shoppingList.setPaid(rs.getBoolean("paid"));
-        shoppingList.setCreatedAt(convertToInstant(rs, "created_at"));
-        shoppingList.setUpdatedAt(convertToInstant(rs, "updated_at"));
-        shoppingList.setCreatedBy(rs.getLong("created_by"));
-        shoppingList.setUpdatedBy(rs.getLong("updated_by"));
-        return shoppingList;
-    }
-
-    private static Instant convertToInstant(ResultSet rs, String columnName) throws SQLException {
-        Object object = rs.getObject(columnName);
-        if (object instanceof java.sql.Timestamp) {
-            return ((java.sql.Timestamp) object).toInstant();
-        } else if (object instanceof java.sql.Date) {
-            return ((java.sql.Date) object).toLocalDate().atStartOfDay().toInstant((ZoneOffset) ZoneId.of("UTC"));
-        }
-        throw new IllegalArgumentException("Unsupported data type for column: " + columnName);
     }
 }
